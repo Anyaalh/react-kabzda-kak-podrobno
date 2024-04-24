ec6684da0d15a102777a5241ce578c2af14895ab
Anyaalh <anya.alh@gmail.com>
2024-04-24 14:56:24 +0300

    react memo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: "ReactMemo demo"
+}
+
+const NewMessageCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const SecretUsers = (props: { users: Array<string> }) => {
+    return (
+        <div>
+            {props.users.map((u, i) => <div key={i}>{u}</div>)}
+        </div>
+    )
+}
+
+const Users = React.memo(SecretUsers)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(["Ann", "Olga"])
+
+    return (
+        <>
+            <button onClick={()=>setCounter(counter+1)}>+</button>
+            <NewMessageCounter count={counter}/>
+            <Users users={users}/>
+        </>
+    )
+}
